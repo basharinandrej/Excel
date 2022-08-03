53e8dbb84c9fa95b4ac2dc4b826651936ac3c391
basharinandrej <basharin_andrej@mail.ru>
2022-08-03 21:15:28 +0300

    create fn constructor for Ecxel class

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,5 +1,15 @@
 import ExcelComponent from '@CORE/ExcelComponent'
 
-class Excel extends ExcelComponent {}
+class Excel extends ExcelComponent {
+    constructor(selector, options) {
+        super()
+        this.$root = document.querySelector(selector)
+        this.collectionComponents = options.components || []
+    }
+
+    render() {
+        return this.$root
+    }
+}
 
 export default Excel
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import '@STYLES/index.sass'
 import Excel from '@Components/excel/Excel'
 
-const excel = new Excel()
+const excel = new Excel('#root')
 
-console.log('Excel', excel)
+console.log('Excel', excel.render())
