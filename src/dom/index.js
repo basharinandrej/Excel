10100f90581bd1b8b2b034d77668986375559a29
basharinandrej <basharin_andrej@mail.ru>
2022-08-07 15:05:09 +0300

    merge folder dom

---
+++ src/dom/index.js
@@ -0,0 +1,33 @@
+class Dom {
+    constructor(selector) {
+        this.$el = typeof selector === 'string' ? document.querySelector(selector) : selector
+    }
+
+    setHTML(html) {
+        if (html instanceof Dom) {
+            html = html.$el
+        }
+        if (typeof html === 'string') {
+            this.$el.innerHTML = html
+        } else {
+            this.$el.append(html)
+        }
+        return this
+    }
+}
+
+export function $(selector) {
+    return new Dom(selector)
+}
+
+$.create = function (tagName, classes) {
+    const node = document.createElement(tagName)
+
+    if (!Array.isArray(classes)) {
+        node.classList.add(classes)
+    } else {
+        node.classList.add(...classes)
+    }
+
+    return $(node)
+}
