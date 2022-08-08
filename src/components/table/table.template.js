f819a5b7ee513c94b40e963d527fc1b421b8a5e3
basharinandrej <basharin_andrej@mail.ru>
2022-08-08 20:13:27 +0300

    replace forEach on new Array()

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -17,18 +17,21 @@ function createCell() {
     `;
 }
 
-function createHeadRow() {
-	const cells = [];
+function toCell(_, index) {
+	const charCode = CODES.A + index;
+	const char = String.fromCharCode(charCode);
+	return createHeadCell(char);
+}
 
-	for (let i = CODES.A; i <= CODES.Z; i++) {
-		const char = String.fromCharCode(i);
-		cells.push(createHeadCell(char));
-	}
+function createHeadRow() {
+	const colsTotal = CODES.Z - CODES.A + 1;
+
+	const cells = new Array(colsTotal).fill("").map(toCell).join("");
 
 	return `
         <div class="table-excel__row-info row-info">
             <div class="row-info__cell row-info__cell--empty"></div>
-            ${cells.join("")}
+            ${cells}
         </div>
     `;
 }
@@ -50,8 +53,8 @@ function createRow(index) {
 
 export function createTable(rowTotal = 15) {
 	const rows = [];
-
 	rows.push(createHeadRow());
+
 	for (let i = 0; i <= rowTotal; i++) {
 		rows.push(createRow(i));
 	}
